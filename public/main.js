5db6fe7132d79bd7b8b36c9435a1d3d6028545c6
jaypajji4444 <43105921+jaypajji4444@users.noreply.github.com>
2019-08-21 19:23:14 +0530

    Rep created

--- public/main.js
+++ public/main.js
@@ -1,5 +1,5 @@
-const api_url="/api";
 
+// ------------------------------ promise handling to fetch..........
 /*
 function getData(){
     fetch(api_url)
@@ -19,31 +19,52 @@ data.forEach(element => {
 getData();
 
 */
+const api_url="/api";
 
 let data;
 let t1;
 
 const getData=async ()=>{
-t1=new Date().getTime();
-    const response=await fetch(api_url);
+     t1=new Date().getTime();// start time
+     const response=await fetch(api_url);
      data=await response.json();
-     console.log(data,(new Date().getTime()-t1)/1000);
-}
-getData();
-setTimeout(()=>{
+     console.log(data,(new Date().getTime()-t1)/1000); // difference of atart time to time after getting the response..
+
+
+setTimeout(()=>{ // displaying the data in html page...
     var container=document.querySelector(".container1");
-    const html=data.map(item=> `<div class="card card-body mb-1 text-center">
+    const html=data.map(item=> `<div class="card card-body mb-1">
   
    <h3><span class="text-primary">Flight No:</span>${item.flight_number}</h3>
     <h4><span class="text-primary">MissionName:</span>${item.mission_name}</h4>
     <h4><span class="text-primary">RocketName:</span>${item.rocket.rocket_name}</h4>
-    <img src=${item.links.mission_patch} style="width:200px;height:200px;">
-    <h5><span class="text-primary">LauchedOn</span>-${new Date(item.launch_date_unix).toLocaleString()}</h5>
+   <div><img src=${item.links.mission_patch} style="width:200px;height:200px;"></div>
+    <h5><span class="text-primary">LauchedOn</span>-${new Date(item.launch_date_local).toLocaleString()}</h5>
    </div>`
     )
   
     container.innerHTML = html;
-},2000)
+},1500)
+
+}
+document.getElementById("btn2").addEventListener("click",getData); // fetching the data on clicking of the button 
+// event listener on button 1 to sote the data to databse......................................
+var button=document.getElementById("btn1");
+button.addEventListener("click",StoreData);
+//  post request from clicenet to sotre the data to database..................
+  function StoreData(){
+    console.log(data);
+
+    fetch("/api",{
+        method: 'post',
+        body: JSON.stringify(data),// data to server shld be string dso convrsion from js object to string..
+        headers: new Headers({ 
+            'Content-Type': 'application/json'
+        })
+
+  }).then(res=>res.json()).then(d=>console.log(d)).catch(err=>console.log(err));
+}
+
 
 
 
--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express=require("express");
 const app=express();
+const bodyParser=require("body-parser");
 
-//const LaunchModel=require("./mongo");
+const LaunchModel=require("./mongo");// data model
 const port=3000;
-const fetch=require("node-fetch");
-app.use(express.urlencoded({extended:true}));
-app.use(express.json());
-app.use(express.static("public"));
+const fetch=require("node-fetch");  //packge for using FETCH()
+app.use(express.json({limit: '50mb'})); // it helps to recognize inclommng object as json object // limit set to 50 to handle large data
+
+app.use(express.urlencoded({limit: '50mb'}));// it helps to recognizeincoming on=bject as  string or arrayss.
+app.use(express.static("public")); // setting up the public folder as static
+
 
 
 app.listen(port,(()=>{
@@ -24,4 +27,24 @@ app.get("/api",(req,res)=>{
 
 app.post("/api",(req,res)=>{
     console.log("Post request from client received....");
+    //console.log(req.body);
+    var jay=req.body;
+    console.log(jay[1].rocket.rocket_/name);
+
+   
+        
+    jay.forEach(element => {
+        const Launch={
+            flightNumber:element.flight_number,
+            rocketName:element.rocket.rocket_name,
+            launchDate:element.launch_date_local,
+            missionName:element.mission_name,
+            missionPatchLink:element.links.mission_patch
+        }
+        var myData = new LaunchModel(Launch);
+        myData.save()
+        .then(item => {res.send("item saved to database");})
+        .catch(err => {res.status(400).send("unable to save to database")});
+        
+    })
 })
